589e61955b1647fe2861d617df2e872cbd5501cb
IsYet1 <Don@IsYet.com>
2021-10-18 14:14:35 -0500

    Basic card created and formatted.

--- SwiftUIGettingStarted/ContentView.swift
+++ SwiftUIGettingStarted/ContentView.swift
@@ -10,9 +10,36 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Color.blue
-            Text("Hello, SwiftUI!")
-                .padding()
+            Rectangle()
+                .fill(Color("CardBackground"))
+                .frame(width: 300, height: 200)
+                .cornerRadius(20)
+                .shadow(radius: 10)
+            VStack(alignment: .leading) {
+                Text("Jonathan Wong")
+                    .font(.title)
+                Text("Senior Software Engineer")
+                    .italic()
+                Spacer()
+                HStack {
+                    Image(systemName: "envelope.fill")
+                        .foregroundColor(.orange)
+                    Text("jonathan@company.com")
+                }
+                HStack {Image(systemName: "link")
+                        .foregroundColor(.orange)
+                    Text("http://Company.com")
+                }
+                HStack {Image(systemName: "location.fill")
+                        .foregroundColor(.orange)
+                    Text("Company address here")
+                }
+                .padding(.bottom)
+
+            }
+            .frame(width: 300, height: 200)
+            .foregroundColor(.white)
+
         }
     }
 }
